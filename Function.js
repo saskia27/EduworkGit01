a6c7d2d879fa687a1ff38f015f151201dd1b2688
sundariputri0201@gmail.com <sundariputri0201@gmail.com>
2024-09-29 02:21:15 +0700

    Initial commit

---
+++ Function.js
@@ -0,0 +1,10 @@
+function myFunction(isGreeting) {
+    if (isGreeting) {
+        alert("Hello World");
+    } else {
+        alert("Goodbye World");
+    }
+}
+
+myFunction(true); // Menampilkan "Hello World"
+myFunction(false); // Menampilkan "Goodbye World"
