e5ee6d73ca6b901a6eab9f86af2909f977d7c72b
sundariputri0201@gmail.com <sundariputri0201@gmail.com>
2024-09-29 02:12:47 +0700

    Initial commit

---
+++ DataTypes.js
@@ -0,0 +1,6 @@
+let length = 16;           // Tipe data: Number (angka)
+let lastname = "Johnson";  // Tipe data: String (teks)
+const x = {                // Tipe data: Object (objek)
+      firstname: "John",   // Properti dalam objek, bertipe String
+      lastname: "Doe"      // Properti dalam objek, bertipe String
+};
