5711c9b706a6d90d57d4e65d7165c172ee43068d
Saskia Juliatika <saskiajuliatika60@gmail.com>
2024-10-16 19:50:32 +0700

    update

--- Condition.js
+++
@@ -1,8 +0,0 @@
-let x = 10; // Ganti nilai x sesuai kebutuhan
-let y = 5;  // Ganti nilai y sesuai kebutuhan
-
-if (x > y) {
-    alert("Hore saya bisa!"); // Pesan yang akan ditampilkan jika x lebih besar dari y
-} else {
-    alert("x tidak lebih besar dari y."); // Pesan alternatif jika kondisi tidak terpenuhi
-}
